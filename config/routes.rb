82a720bd0ca2f198af9eedb9c93779bd413e7c07
gitknkn <atio.asu@gmail.com>
2020-09-04 07:47:30 +0000

    Finish ch07

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,5 @@
 Rails.application.routes.draw do
 
-
   root 'static_pages#home'
   get 'static_pages/home'
   get '/help', to: 'static_pages#help'
@@ -9,6 +8,6 @@ Rails.application.routes.draw do
 
   get '/signup', to: 'users#new'
 
-  # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
-  # root 'application#hello'
+  resources :users
+
 end
